4a1ed5771f3a86971ed0041cc27d32f77f7d01e6
Jhesabel <201803483@est.umss.edu>
2025-10-03 23:48:20 -0400

    ultimos cambios productos

--- tests/api/products.spec.js
+++ tests/api/products.spec.js
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-const BASE_URL = 'https://fakestoreapi.com';
+const BASE_URL = 'https://api.demoblaze.com';
 
+const categorias = ['phone', 'notebook', 'monitor'];
 const productIds = [1, 2, 3, 4, 5]; 
 
-test.describe('API - Productos de Demoblaze', () => {
+/* test.describe('API - Productos de Demoblaze', () => {
 
   test('Listar todos los productos', async ({ request }) => {
     const response = await request.get(`${BASE_URL}/products`);
@@ -30,19 +31,41 @@ test.describe('API - Productos de Demoblaze', () => {
     });
   });
 
-  test('Obtener productos por categoría (electronics)', async ({ request }) => {
-    const response = await request.get(`${BASE_URL}/products/category/electronics`);
-    expect(response.status()).toBe(200);
+});
+ */
+productIds.forEach(id => {
+    test(`Detalle de producto con ID ${id}`, async ({ request }) => {
+      const response = await request.post(`${BASE_URL}/view`, {
+        data: { id: id.toString() } 
+      });
+      expect(response.status()).toBe(200);
 
-    const products = await response.json();
-    console.log("Productos en categoría 'electronics':", products.map(p => p.title));
+      const product = await response.json();
+      console.log(product);
 
-    expect(Array.isArray(products)).toBeTruthy();
-    expect(products.length).toBeGreaterThan(0);
+      expect(product).toHaveProperty('id', id); 
+      expect(product).toHaveProperty('title');
+      expect(product).toHaveProperty('price');
+    });
+  });
+  categorias.forEach(categoria => {
+    test(`Obtener productos por categoría: ${categoria}`, async ({ request }) => {
+      const response = await request.post(`${BASE_URL}/bycat`, {
+        data: { cat: categoria }
+      });
+      expect(response.status()).toBe(200);
 
-    products.forEach(p => {
-      expect(p).toHaveProperty('category', 'electronics');
+      const body = await response.json();
+      console.log(`Categoría: ${categoria}`, body);
+
+      expect(body).toHaveProperty('Items');
+      expect(Array.isArray(body.Items)).toBeTruthy();
+      expect(body.Items.length).toBeGreaterThan(0);
+
+      body.Items.forEach(item => {
+        expect(item).toHaveProperty('cat', categoria);
+      });
     });
   });
 
-});
+
