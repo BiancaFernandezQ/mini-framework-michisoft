cbd7db867c5072cd7274c505c0456c2ef4514293
CarolinaAVar <carocav081@gmail.com>
2025-10-03 21:23:13 -0400

    Commit

--- tests/cardorder.spec.js
+++ tests/cardorder.spec.js
@@ -1,17 +1,13 @@
-// tests/cart.spec.js
 const { test, expect } = require('@playwright/test');
 const { CartPage } = require('../pages/place.order.page.js');
 
 test('Realizar orden desde el carrito', async ({ page }) => {
   const cartPage = new CartPage(page);
 
-  // Ir al carrito
   await page.goto('https://www.demoblaze.com/cart.html');
 
-  // Hacer click en "Place Order"
   await cartPage.clickPlaceOrder();
 
-  // Llenar formulario
   await cartPage.fillOrderForm({
     name: 'Maria',
     country: 'Bolivia',
@@ -21,12 +17,9 @@ test('Realizar orden desde el carrito', async ({ page }) => {
     year: '2025'
   });
 
-  // Enviar orden
   await cartPage.submitOrder();
 
-  // Validar confirmación
   await expect(cartPage.confirmationModal).toBeVisible();
 
-  // Confirmar
   await cartPage.confirmOrder();
 });
--- tests/ui/addproduct.spec.js
+++ tests/ui/addproduct.spec.js
@@ -1,12 +1,45 @@
 import { test, expect } from '../data/product_fixture.js';
+const { test, expect } = require('@playwright/test');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const { SignUp } = require('../pages/sign_up');
+import { expect } from "@playwright/test";
+import { LoginPage } from "../../pages/login_page.js";
+import { test } from "@playwright/test";
+
+test.describe("Login Page", () => {
+    let loginPage;
+
+    test.beforeEach(async ({ page }) => {
+        console.log("Se ejecuta ANTES de cada test");
+        loginPage = new LoginPage(page);
+        await loginPage.goto();
+
+    });
+
+    test.afterEach(async () => {
+        console.log("Se ejecuta DESPUÉS de cada test");
+    });
+
+
+    test("Validar que el loguin fue exitoso", async ({ page }) => {
+        await loginPage.login("danielab", "1234");
+        //Validar que la lista de productos está visible
+        await expect(loginPage.products_list).toBeVisible();
+
+    });
+
+
+});
 
 test('Flujo E2E productos', async ({ homePage, productPage }) => {
-  const productName = 'Samsung galaxy s6';
 
-  await homePage.goto();
-  await homePage.selectProduct(productName);
+    const productName = 'Samsung galaxy s6';
 
-  const alertMessage = await productPage.addToCartAndAcceptAlert();
+    await homePage.goto();
+    await homePage.selectProduct(productName);
 
-  expect(alertMessage).toBe('Product added.');
+    const alertMessage = await productPage.addToCartAndAcceptAlert();
+
+    expect(alertMessage).toBe('Product added.');
 });
